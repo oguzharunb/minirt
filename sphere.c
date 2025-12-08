68a8545d94c6e4e5c9008dbf3f6f8c7ce70eb547
Muhammed Sengul <msngl@icloud.com>
2025-12-08 09:58:00 +0300

    Sphere kesişim fonksiyonunu yeni ray yapısına göre güncelledim

--- sphere.c
+++ sphere.c
@@ -1,44 +1,31 @@
 #include "objects.h"
 #include "ray.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 t_sphere	*create_sphere(double radius, double x, double y, double z)
 {
-	t_sphere	*sphere;
-	t_point3	*center;
+	t_sphere	*s;
 
-	sphere = malloc(sizeof(t_sphere));
-	if (!sphere)
+	s = malloc(sizeof(t_sphere));
+	if (!s)
 		return (NULL);
-	center = create_vector(x, y, z);
-	if (!center)
-	{
-		free(sphere);
-		return (NULL);
-	}
-	sphere->center = center;
-	sphere->radius = radius;
-	return (sphere);
+	s->radius = radius;
+	s->center = vec(x, y, z);
+	return (s);
 }
 
-int	hit_sphere(t_sphere *sphere, t_ray *ray)
+int	hit_sphere(t_sphere *s, t_ray *ray)
 {
-	t_vec3 *oc;
-	double a;
-	double b;
-	double c;
-	double discriminant;
-	int result;
+	t_vec3	oc;
+	double	a;
+	double	half_b;
+	double	c;
+	double	discriminant;
 
-	oc = vector_subtract(ray->point, sphere->center);
-	if (!oc)
-		return (-1);
-	a = dot_product2(ray->vec, ray->vec);
-	b = -2.0 * dot_product2(ray->vec, oc);
-	c = dot_product2(oc, oc) - sphere->radius * sphere->radius;
-	discriminant = b * b - 4 * a * c;
-	result = (discriminant >= 0);
-	free(oc);
-	return (result);
+	oc = vec_sub(ray->origin, s->center);
+	a = vec_dot(ray->direction, ray->direction);
+	half_b = vec_dot(oc, ray->direction);
+	c = vec_dot(oc, oc) - s->radius * s->radius;
+	discriminant = half_b * half_b - a * c;
+	return (discriminant >= 0);
 }

af17034f61cf892676569a3d0dd871ae23d0f994
Muhammed Sengul <msngl@icloud.com>
2025-12-08 09:56:10 +0300

    Temel vektör kütüphanesini yeniden yazdım (vec_add, vec_sub, normalize vb.)

--- vector.c
+++ vector.c
@@ -1,16 +1,53 @@
 #include "vector.h"
+#include <math.h>
 #include <stdlib.h>
 
-t_vec3	*create_vector(double x, double y, double z)
+t_vec3	vec(double x, double y, double z)
 {
-	t_vec3	*vector;
+	t_vec3	v;
 
-	vector = malloc(sizeof(t_vec3));
-	if (!vector)
-		return (NULL);
-	vector->x = x;
-	vector->y = y;
-	vector->z = z;
-	return (vector);
+	v.x = x;
+	v.y = y;
+	v.z = z;
+	return (v);
+}
+
+t_vec3	vec_add(t_vec3 a, t_vec3 b)
+{
+	return (vec(a.x + b.x, a.y + b.y, a.z + b.z));
+}
+
+t_vec3	vec_sub(t_vec3 a, t_vec3 b)
+{
+	return (vec(a.x - b.x, a.y - b.y, a.z - b.z));
+}
+
+t_vec3	vec_mul(t_vec3 a, double s)
+{
+	return (vec(a.x * s, a.y * s, a.z * s));
+}
+
+double	vec_dot(t_vec3 a, t_vec3 b)
+{
+	return (a.x * b.x + a.y * b.y + a.z * b.z);
+}
+t_vec3	vec_cross(t_vec3 a, t_vec3 b)
+{
+	return (vec(
+		a.y * b.z - a.z * b.y,
+		a.z * b.x - a.x * b.z,
+		a.x * b.y - a.y * b.x
+	));
+}
+double	vec_length(t_vec3 a)
+{
+	return (sqrt(vec_dot(a, a)));
+}
+t_vec3	vec_normalize(t_vec3 a)
+{
+	double len;
+
+	len = vec_length(a);
+	return (vec(a.x / len, a.y / len, a.z / len));
 }
 
--- vector.h
+++ vector.h
@@ -2,18 +2,25 @@
 # define VECTOR_H
 
 typedef struct s_vec {
-	double	x;
-	double	y;
-	double	z;
-}	t_vec3;
+    double x;
+    double y;
+    double z;
+}   t_vec3;
 
 typedef t_vec3 t_point3;
 
-t_vec3	*scale_vector(t_vec3 *vec, double scale_by);
-t_vec3	*create_vector(double x, double y, double z);
-double	dot_product3(t_vec3 *vec1, t_vec3 *vec2, t_vec3 *vec3);
-double	dot_product2(t_vec3 *vec1, t_vec3 *vec2);
-t_vec3	*vector_sum(t_vec3 *vec1, t_vec3 *vec2);
-t_vec3	*vector_subtract(t_vec3 *vec1, t_vec3 *vec2);
+
+t_vec3      vec(double x, double y, double z);
+
+
+t_vec3      vec_add(t_vec3 a, t_vec3 b);
+t_vec3      vec_sub(t_vec3 a, t_vec3 b);
+t_vec3      vec_mul(t_vec3 a, double s);
+
+
+double      vec_dot(t_vec3 a, t_vec3 b);
+t_vec3      vec_cross(t_vec3 a, t_vec3 b);
+double      vec_length(t_vec3 a);
+t_vec3      vec_normalize(t_vec3 a);
 
 #endif

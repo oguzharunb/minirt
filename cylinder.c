96a4c4c82dafe79614657bbf58616794468a2024
msengul <msngl0234@gmail.com>
2025-12-23 17:01:29 +0300

    cylinder dosyasi norm'a gore fonksiyonlara bolundu ve yeni struct eklendi artik norm'a uyuyo

--- cylinder.c
+++ cylinder.c
@@ -1,95 +1,116 @@
-#include "objects.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cylinder.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msengul <msengul@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 16:56:36 by msengul           #+#    #+#             */
+/*   Updated: 2025/12/23 16:59:44 by msengul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
-#include <stdlib.h>
+#include "objects.h"
 #include <math.h>
+#include <stdlib.h>
 
-int hit_disk(t_ray *ray, t_vec3 center, t_vec3 normal, double radius, double *t)
+typedef struct s_cy_vars
 {
-    double  denom;
-    double  num;
-    double  t_temp;
-    t_vec3  p;
-    t_vec3  v;
+	t_vec3	oc;
+	double	a;
+	double	b;
+	double	c;
+	double	discr;
+	double	t1;
+	double	t2;
+	double	min_t;
+}			t_cy_vars;
 
-    denom = vec_dot(ray->direction, normal);
-    if (fabs(denom) < 1e-6) 
-        return (0);
-    t_vec3 oc = vec_sub(center, ray->origin);
-    num = vec_dot(oc, normal);
-    t_temp = num / denom;
-    if (t_temp < 1e-4)
-        return (0);
-    p = vec_add(ray->origin, vec_mul(ray->direction, t_temp));
-    v = vec_sub(p, center);
-    if (vec_dot(v, v) <= radius * radius)
-    {
-        *t = t_temp;
-        return (1);
-    }
-    return (0);
+int	hit_disk(t_ray *ray, t_plane pl, double radius, double *t)
+{
+	double	denom;
+	double	t_temp;
+	t_vec3	p;
+	t_vec3	v;
+
+	denom = vec_dot(ray->direction, pl.normal);
+	if (fabs(denom) < 1e-6)
+		return (0);
+	t_temp = vec_dot(vec_sub(pl.point, ray->origin), pl.normal) / denom;
+	if (t_temp < 1e-4)
+		return (0);
+	p = vec_add(ray->origin, vec_mul(ray->direction, t_temp));
+	v = vec_sub(p, pl.point);
+	if (vec_dot(v, v) <= radius * radius)
+	{
+		*t = t_temp;
+		return (1);
+	}
+	return (0);
 }
 
-int hit_cylinder(t_cylinder *cy, t_ray *ray, double *t)
+static void	check_body_hit(t_cylinder *cy, t_ray *ray, t_cy_vars *v)
 {
-    double  t_body = -1.0;
-    double  t_top = -1.0;
-    double  t_bottom = -1.0;
-    int     hit_any = 0;
-    
-    t_vec3  oc = vec_sub(ray->origin, cy->center);
-    double  dot_r_axis = vec_dot(ray->direction, cy->axis);
-    double  dot_oc_axis = vec_dot(oc, cy->axis);
-    double  a = vec_dot(ray->direction, ray->direction) - (dot_r_axis * dot_r_axis);
-    double  b = 2 * (vec_dot(ray->direction, oc) - (dot_r_axis * dot_oc_axis));
-    double  c = vec_dot(oc, oc) - (dot_oc_axis * dot_oc_axis) - (cy->radius * cy->radius);
-    double  discriminant = b * b - 4 * a * c;
+	double	dot_r_ax;
+	double	dot_oc_ax;
 
-    if (discriminant >= 0)
-    {
-        double t1 = (-b - sqrt(discriminant)) / (2 * a);
-        double t2 = (-b + sqrt(discriminant)) / (2 * a);
-        double m;
+	dot_r_ax = vec_dot(ray->direction, cy->axis);
+	dot_oc_ax = vec_dot(v->oc, cy->axis);
+	v->a = vec_dot(ray->direction, ray->direction) - (dot_r_ax * dot_r_ax);
+	v->b = 2 * (vec_dot(ray->direction, v->oc) - (dot_r_ax * dot_oc_ax));
+	v->c = vec_dot(v->oc, v->oc) - (dot_oc_ax * dot_oc_ax) - (cy->radius
+			* cy->radius);
+	v->discr = v->b * v->b - 4 * v->a * v->c;
+	if (v->discr >= 0)
+	{
+		v->t1 = (-v->b - sqrt(v->discr)) / (2 * v->a);
+		v->t2 = (-v->b + sqrt(v->discr)) / (2 * v->a);
+		if (v->t1 > 1e-4 && ((dot_r_ax * v->t1 + dot_oc_ax) >= -cy->height / 2.0
+				&& (dot_r_ax * v->t1 + dot_oc_ax) <= cy->height / 2.0))
+			if (v->t1 < v->min_t)
+				v->min_t = v->t1;
+		if (v->t2 > 1e-4 && ((dot_r_ax * v->t2 + dot_oc_ax) >= -cy->height / 2.0
+				&& (dot_r_ax * v->t2 + dot_oc_ax) <= cy->height / 2.0))
+			if (v->t2 < v->min_t)
+				v->min_t = v->t2;
+	}
+}
 
-        if (t1 > 1e-4)
-        {
-            m = dot_r_axis * t1 + dot_oc_axis;
-            if (m >= -cy->height / 2.0 && m <= cy->height / 2.0)
-            {
-                t_body = t1;
-                hit_any = 1;
-            }
-        }
-        if (t_body == -1.0 && t2 > 1e-4)
-        {
-            m = dot_r_axis * t2 + dot_oc_axis;
-            if (m >= -cy->height / 2.0 && m <= cy->height / 2.0)
-            {
-                t_body = t2;
-                hit_any = 1;
-            }
-        }
-    }
-    t_vec3 top_center = vec_add(cy->center, vec_mul(cy->axis, cy->height / 2.0));
-    if (hit_disk(ray, top_center, cy->axis, cy->radius, &t_top))
-        hit_any = 1;
-    t_vec3 bot_center = vec_sub(cy->center, vec_mul(cy->axis, cy->height / 2.0));
-    t_vec3 bot_axis = vec_mul(cy->axis, -1.0); 
-    if (hit_disk(ray, bot_center, bot_axis, cy->radius, &t_bottom))
-        hit_any = 1;
-    if (!hit_any)
-        return (0);
-    double min_t = 1e30; 
-    int found = 0;
+static void	check_caps_hit(t_cylinder *cy, t_ray *ray, double *min_t)
+{
+	t_plane	cap;
+	double	t_cap;
 
-    if (t_body > 1e-4 && t_body < min_t) { min_t = t_body; found = 1; }
-    if (t_top > 1e-4 && t_top < min_t)   { min_t = t_top; found = 1; }
-    if (t_bottom > 1e-4 && t_bottom < min_t) { min_t = t_bottom; found = 1; }
+	t_cap = 0;
+	cap.point = vec_add(cy->center, vec_mul(cy->axis, cy->height / 2.0));
+	cap.normal = cy->axis;
+	if (hit_disk(ray, cap, cy->radius, &t_cap))
+	{
+		if (t_cap < *min_t)
+			*min_t = t_cap;
+	}
+	cap.point = vec_sub(cy->center, vec_mul(cy->axis, cy->height / 2.0));
+	cap.normal = vec_mul(cy->axis, -1.0);
+	if (hit_disk(ray, cap, cy->radius, &t_cap))
+	{
+		if (t_cap < *min_t)
+			*min_t = t_cap;
+	}
+}
 
-    if (found)
-    {
-        *t = min_t;
-        return (1);
-    }
-    
-    return (0);
+int	hit_cylinder(t_cylinder *cy, t_ray *ray, double *t)
+{
+	t_cy_vars	v;
+
+	v.min_t = 1e30;
+	v.oc = vec_sub(ray->origin, cy->center);
+	check_body_hit(cy, ray, &v);
+	check_caps_hit(cy, ray, &v.min_t);
+	if (v.min_t < 1e30)
+	{
+		*t = v.min_t;
+		return (1);
+	}
+	return (0);
 }

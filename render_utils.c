67f696a0901419065b10aed02f41c77d41906443
msengul <msngl0234@gmail.com>
2025-12-24 13:08:07 +0300

    render_utils.c icerisinde golge kontrolu eklendi.render.c dosyasindaki is_in_shadow_scene fonksiyonu bu dosyaya tasindi

--- render_utils.c
+++ render_utils.c
@@ -6,11 +6,12 @@
 /*   By: msengul <msengul@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 12:53:10 by msengul           #+#    #+#             */
-/*   Updated: 2025/12/24 12:59:09 by msengul          ###   ########.fr       */
+/*   Updated: 2025/12/24 13:07:28 by msengul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+#define EPS 1e-4
 
 static int	clamp255(int v)
 {
@@ -41,4 +42,24 @@ int	get_obj_color(t_hit *hit)
 		return (rgb_to_int(((t_plane *)hit->obj)->color));
 	else
 		return (rgb_to_int(((t_cylinder *)hit->obj)->color));
+}
+
+int	is_in_shadow_scene(t_scene *scene, t_vec3 p, t_vec3 n, t_vec3 light_pos)
+{
+	t_ray	shadow_ray;
+	t_hit	hit;
+	t_vec3	to_light;
+	double	dist;
+
+	to_light = vec_sub(light_pos, p);
+	dist = vec_length(to_light);
+	if (dist <= EPS)
+		return (0);
+	shadow_ray.origin = vec_add(p, vec_mul(n, EPS));
+	shadow_ray.direction = vec_mul(to_light, 1.0 / dist);
+	if (!scene_closest_hit(scene, &shadow_ray, &hit))
+		return (0);
+	if (hit.t > EPS && hit.t < dist - EPS)
+		return (1);
+	return (0);
 }

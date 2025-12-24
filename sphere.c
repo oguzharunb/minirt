6e63a3287161d7656c5b2475a121a8c7c4cd0e64
msengul <msngl0234@gmail.com>
2025-12-24 12:05:00 +0300

    sphere.c dosyasindaki hit_sphere fonksiyonu norm'a gore revize edildi eski hali simdilik duruyo. dosya norma uygun hale geldi kontrol sonrasi eski hit_sphere fonksiyon kaldirilacak

--- sphere.c
+++ sphere.c
@@ -6,7 +6,7 @@
 /*   By: msengul <msengul@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 16:29:16 by msengul           #+#    #+#             */
-/*   Updated: 2025/12/24 00:53:12 by msengul          ###   ########.fr       */
+/*   Updated: 2025/12/24 12:01:21 by msengul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,75 @@
 #include "ray.h"
 #include <math.h>
 
+#define EPS 1e-4
+
+static int	pick_root(double half_b, double a, double sqrt_d, double *t)
+{
+	double	root;
+
+	root = (-half_b - sqrt_d) / a;
+	if (root > EPS)
+	{
+		*t = root;
+		return (1);
+	}
+	root = (-half_b + sqrt_d) / a;
+	if (root > EPS)
+	{
+		*t = root;
+		return (1);
+	}
+	return (0);
+}
+
 int	hit_sphere(t_sphere *s, t_ray *ray, double *t)
 {
-	const double	EPSILON = 1e-4;
-	t_vec3			oc;
-	double			a;
-	double			half_b;
-	double			c;
-	double			discriminant;
-	double			sqrt_d;
-	double			root;
+	t_vec3	oc;
+	double	a;
+	double	half_b;
+	double	c;
+	double	d;
 
 	oc = vec_sub(ray->origin, s->center);
 	a = vec_dot(ray->direction, ray->direction);
 	if (fabs(a) < 1e-12)
 		return (0);
 	half_b = vec_dot(oc, ray->direction);
-	c = vec_dot(oc, oc) - s->radius * s->radius;
-	discriminant = half_b * half_b - a * c;
-	if (discriminant < 0)
+	c = vec_dot(oc, oc) - (s->radius * s->radius);
+	d = (half_b * half_b) - (a * c);
+	if (d < 0.0)
 		return (0);
-	sqrt_d = sqrt(discriminant);
-	root = (-half_b - sqrt_d) / a;
-	if (root <= EPSILON)
-	{
-		root = (-half_b + sqrt_d) / a;
-		if (root <= EPSILON)
-			return (0);
-	}
-	*t = root;
-	return (1);
+	return (pick_root(half_b, a, sqrt(d), t));
 }
+
+// int	hit_sphere(t_sphere *s, t_ray *ray, double *t)
+// {
+// 	const double	EPSILON = 1e-4;
+// 	t_vec3			oc;
+// 	double			a;
+// 	double			half_b;
+// 	double			c;
+// 	double			discriminant;
+// 	double			sqrt_d;
+// 	double			root;
+
+// 	oc = vec_sub(ray->origin, s->center);
+// 	a = vec_dot(ray->direction, ray->direction);
+// 	if (fabs(a) < 1e-12)
+// 		return (0);
+// 	half_b = vec_dot(oc, ray->direction);
+// 	c = vec_dot(oc, oc) - s->radius * s->radius;
+// 	discriminant = half_b * half_b - a * c;
+// 	if (discriminant < 0)
+// 		return (0);
+// 	sqrt_d = sqrt(discriminant);
+// 	root = (-half_b - sqrt_d) / a;
+// 	if (root <= EPSILON)
+// 	{
+// 		root = (-half_b + sqrt_d) / a;
+// 		if (root <= EPSILON)
+// 			return (0);
+// 	}
+// 	*t = root;
+// 	return (1);
+// }
